8a7e62352cb9250d212314762c696894d4052797
Md Razu Ahamad <mdrazuahamad8@gmail.com>
2024-05-09 19:44:51 +0600

    new

--- server.js
+++ server.js
@@ -1,78 +1,9 @@
 const express = require('express')
-
-const app = express();
-app.use(express.json());
-
-const books = [
-    {
-        id: '1',
-        name: 'Full-Stack Development',
-        price: 500
-    },
-    {
-        id: '2',
-        name: 'JavaScript',
-        price: 1000
-    },
-    {
-        id: '3',
-        name: 'typScript Solutions',
-        price: 1500
-    },
-    {
-        id: '4',
-        name: 'React Js',
-        price: 2000
-    },
-    {
-        id: '1',
-        name: 'Full-Stack Development',
-        price: 5000
-    },
-    {
-        id: '2',
-        name: 'JavaScript',
-        price: 100
-    },
-    {
-        id: '3',
-        name: 'typScript Solutions',
-        price: 150
-    },
-    {
-        id: '4',
-        name: 'React Js',
-        price: 200
-    }
-
-]
-
-app.get('/books', (req, res) => {
-    if (req.query.show === 'all') {
-        return res.json(books);
-    }
-    if(req.query.price == '500'){
-        const result = books.filter((books) => books.price <= 500);
-        return res.json(result)
-    }
-    if(req.query.price == '1000'){
-        const result = books.filter((books)=> books.price <= 1000);
-        return res.json(result)
-
-    }
-
-    return res.json(books);
-})
+const app = express()
 
 
-app.post('/books', (req,res)=> {
-    const book = req.body;
-    books.push(book);
-    res.json(books);
-
-})
 
 
-app.listen(8000, () => {
-    console.log('Server is listening on port 8000')
+app.listen(8000,() => {
+    console.log('Applications running on PORT 8000  ');
 })
